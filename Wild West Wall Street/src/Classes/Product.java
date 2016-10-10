3e2c41837622b2982e3b41522766467c047868d7
1dani <1dani@DANIEL_MSI>
2016-10-10 13:39:09 -0600

    10-10-16 Product

--- Wild West Wall Street/src/Classes/Product.java
+++ Wild West Wall Street/src/Classes/Product.java
@@ -14,4 +14,63 @@ import java.io.Serializable;
  */
 public class Product implements Serializable{
     
+    private double value;
+    private double noInStock;
+
+    public Product() {
+    }
+    
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getNoInStock() {
+        return noInStock;
+    }
+
+    public void setNoInStock(double noInStock) {
+        this.noInStock = noInStock;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.value) ^ (Double.doubleToLongBits(this.value) >>> 32));
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.noInStock) ^ (Double.doubleToLongBits(this.noInStock) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        if (Double.doubleToLongBits(this.value) != Double.doubleToLongBits(other.value)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.noInStock) != Double.doubleToLongBits(other.noInStock)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "value=" + value + ", noInStock=" + noInStock + '}';
+    }
+    
+    
+    
 }
