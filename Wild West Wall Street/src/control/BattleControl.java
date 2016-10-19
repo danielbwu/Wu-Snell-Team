e817c531e4c85f335fc297d579f048746401395c
1dani <1dani@DANIEL_MSI>
2016-10-19 09:59:12 -0600

    10-18-16 Created BankControl class; Added calcMonthlyPayment

---
+++ Wild West Wall Street/src/control/BattleControl.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author 1dani
+ */
+public class BattleControl {
+    
+    public Boolean hitTarget(int weaponRange, int targetDistance){
+        if (targetDistance == 0 && weaponRange != 0) //0 is melee range. Melee weapons only
+            return false;
+        else if (targetDistance < 0 || weaponRange < 0)
+            return false;
+        else if (targetDistance > 3 || weaponRange > 3)
+            return false;
+        else if (targetDistance % 1 != 0 || weaponRange % 1 != 0)
+            return false;
+        else if (weaponRange >= targetDistance)
+            return true;
+        else
+            return false;
+    }
+    
+    public double attackTarget(double targetHealth, double weaponDamage, int weaponRange, int targetDistance){
+        /*
+        call hitTarget()
+        if true subtract damage from target health
+        if false return -1
+        */
+    }
+}
