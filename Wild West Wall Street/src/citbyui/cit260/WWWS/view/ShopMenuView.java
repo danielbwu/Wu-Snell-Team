94f14d2d8f7b08441c9d0952594669b3c2de4661
1dani <1dani@DANIEL_MSI>
2016-11-01 09:53:35 -0600

    11-1-16 Added ShopMenuView

--- Wild West Wall Street/src/citbyui/cit260/WWWS/view/ShopMenuView.java
+++ Wild West Wall Street/src/citbyui/cit260/WWWS/view/ShopMenuView.java
@@ -14,5 +14,81 @@ import java.util.Scanner;
  * @author Daniel
  */
 public class ShopMenuView {
+    private String menu;
+    private String promptMessage;
     
+    public ShopMenuView(){ //Initial View
+        this.menu = "\n"
+                    +"\n----------------------------------"
+                    +"\n| Inventory                      |"
+                    +"\n----------------------------------"
+                    +"\nB - Buy"
+                    +"\nS - Sell"
+                    +"\nQ - Exit Menu"
+                    +"\n----------------------------------";
+        this.promptMessage = "\nPlease select an option: ";
+    }
+    
+     public void displayIShopMenuView() { //Calls functions to display initial view and prompt input
+       
+      boolean done = false;
+      do {
+          String shopMenuOption =this.getShopMenuOption();
+          if (shopMenuOption.toUpperCase().equals("Q"))
+              return;
+          
+          done = this.doAction(shopMenuOption);
+          
+      }while(!done);
+    }
+
+    private String getShopMenuOption() { //Gets input
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+        
+        while (!valid){
+            System.out.println("\n" + this.promptMessage +this.menu);
+           
+            value = keyboard.nextLine();
+            value = value.trim();
+            
+           
+            if(value.length() <1) {
+                System.out.println("\nInvalid vaule: value can not be blank");
+                continue;    
+                
+            }
+            break;
+       
+        }
+        return value;
+    }
+    
+    private boolean doAction(String choice) {
+      choice = choice.toUpperCase();
+      
+      switch(choice){
+          case "B":
+              this.buyItems();
+              break;
+          case "S":
+              this.sellItems();
+              break;
+          default:
+              System.out.println("\n*** Invalid selection *** Try again");
+              break;
+      }
+      
+      return false;
+    }
+    
+    private void buyItems() {
+        System.out.println("buyItems() calld");
+       
+    }
+    private void sellItems() {
+        System.out.println("sellItems calld");
+       
+    }
 }
