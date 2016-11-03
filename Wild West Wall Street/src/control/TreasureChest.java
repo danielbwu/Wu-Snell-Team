6933ae2611bcad0b5eb091575708e432f50ac191
Kameron <Kameron@KameronSnell>
2016-11-03 09:54:33 -0600

    11-2-2016 gameMenu in mainMenu

--- Wild West Wall Street/src/control/TreasureChest.java
+++ Wild West Wall Street/src/control/TreasureChest.java
@@ -15,10 +15,15 @@ public class TreasureChest {
        
     public static double calcVolumeOfCone(double height, double diameter){
        
-        if (height < 0)
-            return -1;
-        if (diameter < 0 || diameter >12)
-            return -1;
+        if (height < 1){
+            System.out.println("\nInvalid input: Height must be higher then 0.");
+        return -1;
+        }
+        if (diameter < 0 || diameter >12){
+            System.out.println("Invalid input: Diameter must be between 1 and 12.");
+                return -1;
+        }
+        
         
         double radius = diameter/2;
         double volume = ((1.0/3.0) * PI * radius * radius * height);
