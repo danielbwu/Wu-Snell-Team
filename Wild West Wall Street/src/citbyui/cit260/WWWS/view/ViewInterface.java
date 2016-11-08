10ec31798ab356a1d209b13f918ede37b6e1f483
1dani <1dani@DANIEL_MSI>
2016-11-08 09:32:30 -0700

    11-8-16 - ViewInterface

---
+++ Wild West Wall Street/src/citbyui/cit260/WWWS/view/ViewInterface.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.WWWS.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Daniel
+ */
+public interface ViewInterface {
+    
+
+    
+   /* public String getInput() {
+
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+
+        while (!valid) {
+          //  System.out.println("\n" + this.promptMessage);
+
+            value = keyboard.nextLine();
+
+            if (value.length() < 1) {
+                System.out.println("\nInvalid vaule: value can not be blank");
+                continue;
+            }
+            break;
+        }
+        return value;
+    }*/
+}
