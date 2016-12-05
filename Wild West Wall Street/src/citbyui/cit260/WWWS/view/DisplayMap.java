7790ef5c2d9f39c5f426a6b7d82f02ad90828ea9
Kameron <Kameron@KameronSnell>
2016-12-05 09:13:51 -0700

    MAP!!!!!!!!!!

--- Wild West Wall Street/src/citbyui/cit260/WWWS/view/DisplayMap.java
+++ Wild West Wall Street/src/citbyui/cit260/WWWS/view/DisplayMap.java
@@ -11,40 +11,34 @@ import Classes.Location;
 import cit260.game.CIT260Game;
 import control.MapControl;
 
-
 /**
  *
  * @author Kameron
  */
 public class DisplayMap {
 
+    public void displayMap() {
 
+        Location[][] locations = CIT260Game.getCurrentGame().getMap().getLocations();
 
-    
-    public void displayMap(){
-        
-       Location[][] locations = CIT260Game.getCurrentGame().getMap().getLocations();
-        
         System.out.println("           Wild Wild West");
         System.out.println("   1  2  3  4  5  6  7  8  9  10\n"
                 + "-------------------------------------");
-       for(int row = 0;row< locations.length;row++){
-           System.out.println(row + 1);
+        for (int row = 0; row < locations.length; row++) {
+            System.out.print("\n" + (row + 1));
             System.out.print("|  ");
-            for(int column = 0;column < locations[row].length; column++){
-              System.out.println("----------------------------");
+            
+            for (int column = 0; column < locations[row].length; column++) {
                 
-              Location location = locations[row][column];
-              if(location.isVisited()){
-              System.out.print(location.getScene().getDisplaySymbol());
-              }
-                      
-              else{
-                  System.out.print ("??");
-              }
-             
-    }
-    
-       }
+                Location location = locations[row][column];
+                if (location.isVisited()) {
+                    System.out.print(location.getScene().getDisplaySymbol());
+                } else {
+                    System.out.print("??");
+                }
+                   System.out.print("|");
+            }
+            System.out.print("\n" + "----------------------------------------");
+        }
     }
 }
