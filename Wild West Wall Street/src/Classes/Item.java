d306ff6a4ca25a3e58154507b3bd992f0e18f212
1dani <1dani@DANIEL_MSI>
2016-12-08 09:18:25 -0700

    12-8-16 - Notes

--- Wild West Wall Street/src/Classes/Item.java
+++ Wild West Wall Street/src/Classes/Item.java
@@ -2,6 +2,12 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * What does the map do?
+What does map have?
+What does player have?
+What does player do?
+What does each location have?
+How do these all interact?
  */
 package Classes;
 
